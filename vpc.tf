c189b78e687caf31b311f1e7612ee318062a61f5
Jonathan Sabo <jonathan@luminal.io>
2018-04-18 08:27:14 -0400

    wip

---
+++ vpc.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/Users/Jonathan/.aws/credentials"
+  profile                 = "business-unit-security"
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+}
